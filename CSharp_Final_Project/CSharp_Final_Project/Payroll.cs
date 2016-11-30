278bc4f2b76c6a92398f68ce5109bec154362877
Ryan Brooks <10thTARDIS@users.noreply.github.com>
2016-11-29 18:24:34 -0600

    Setup project

---
+++ CSharp_Final_Project/CSharp_Final_Project/Payroll.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Final_Project
+{
+    class Payroll
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public void menu()
+        {
+
+        }
+
+        public void populateEmployees()
+        {
+
+        }
+
+        public void selectEmployee()
+        {
+
+        }
+
+        public void saveEmployee()
+        {
+
+        }
+
+        public void loadEmployee()
+        {
+
+        }
+    }
+}
