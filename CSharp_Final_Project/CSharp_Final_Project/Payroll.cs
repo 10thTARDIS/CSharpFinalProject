6fd44e279b8c6d11173b93894ef91447f205b706
Ryan Brooks <ryan@experimentalthoughts.com>
2016-11-29 18:50:27 -0600

    Made a start on the menu.

--- CSharp_Final_Project/CSharp_Final_Project/Payroll.cs
+++ CSharp_Final_Project/CSharp_Final_Project/Payroll.cs
@@ -8,6 +8,8 @@ namespace CSharp_Final_Project
 {
     class Payroll
     {
+        int input;
+        bool employeesExist = false;
         public static void Main(string[] args)
         {
 
@@ -15,12 +17,41 @@ namespace CSharp_Final_Project
 
         public void menu()
         {
+            input = -99;
+            Console.WriteLine("Welcome to the system.");
+            Console.WriteLine("\nWhat would you like to do?\n");
+            Console.WriteLine("1) Populate Employees");
+            Console.WriteLine("2) Select Employee");
+            Console.WriteLine("3) Save Employees");
+            Console.WriteLine("4) Load Employees");
+            Console.WriteLine("5) Exit");
+            input = Convert.ToInt32(Console.ReadLine());
+            if (input == 1 && employeesExist == false)
+            {
+                populateEmployees();
+            }
+            else if (input == 2 && employeesExist == true)
+            {
+
+            }
+            else if (input == 3 && employeesExist == true)
+            {
+
+            }
+            else if (input == 3 && employeesExist == true)
+            {
+
+            }
+            else if (input == 4 && employeesExist == false)
+            {
+
+            }
 
         }
 
         public void populateEmployees()
         {
-
+            employeesExist = true;
         }
 
         public void selectEmployee()
