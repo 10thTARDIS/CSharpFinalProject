2daba41f6f836ccc04319f8268ee4ae544b49ca6
Lizzy <eathd3@mail.umsl.edu>
2016-12-01 20:36:28 -0600

    menu update

--- CSharp_Final_Project/CSharp_Final_Project/Employee.cs
+++ CSharp_Final_Project/CSharp_Final_Project/Employee.cs
@@ -35,7 +35,39 @@ namespace CSharp_Final_Project
         ********************/
         public void menu()
         {
+            int input;
+            do
+            {
+                Console.WriteLine("\nWhat would you like to do?\n");
+                Console.WriteLine("1) Calculate Gross Pay");
+                Console.WriteLine("2) Calculate Tax");
+                Console.WriteLine("3) Calculate Net Pay");
+                Console.WriteLine("4) Calculate Net Percent");
+                Console.WriteLine("4) Display Employee ");
+                Console.WriteLine("6) Exit");
+                input = Convert.ToInt32(Console.ReadLine());
+                if (input == 1)
+                {
+                    computeGross();
+                }
+                else if (input == 2)
+                {
+                    computeTax();
+                }
+                else if (input == 3)
+                {
+                    computeNet();
+                }
+                else if (input == 4)
+                {
+                    computeNetperc();
+                }
+                else if (input == 5)
+                {
+                    displayEmployee();
+                }
 
+            } while (input != 6);
         }
 
         public void computeGross()
